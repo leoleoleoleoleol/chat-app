d98e18a56168f29cb8dfb476b58a15bcdab0a63c
tsumano <tsumano@fundely.co.jp>
2024-02-06 11:11:28 +0900

    add:定義付けの追加

--- next.config.js
+++ next.config.js
@@ -2,3 +2,12 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+const path = require('path');
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
