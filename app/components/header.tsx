cae1012d7517cccaac4b931b7a530c2ad9ac25a3
tsumano <tsumano@fundely.co.jp>
2024-02-06 13:23:50 +0900

    add:ロゴ画像の追加

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
-
+import Image from 'next/image';
 export default function Header() {
     return (
         <header className="p-4 border-b-2 border-gray-300 fixed w-full bg-white">
             <ul className="w-full max-w-xl m-auto flex font-medium flex-row">
-                <li>
-                    <Link className="text-gray-700 hover:text-blue-700" href="/">Home</Link>
-                </li>
+            <Link className="text-gray-700 hover:text-blue-700" href="/">
+            <Image src="/fundely.jpg" alt="logo"width={100} height={30}/>
+                </Link> 
             </ul>
         </header>
     )
