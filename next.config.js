2722d94111615e1d94c87a4b30d040f8a2774fa5
tsumano <tsumano@fundely.co.jp>
2024-02-26 13:56:31 +0900

    change:定義付けの変更

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+}
 
 const path = require('path');
 
